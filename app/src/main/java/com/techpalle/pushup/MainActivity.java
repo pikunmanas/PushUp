9c6bfa7b63bb04efad493c2d9a0f8fe6ea6a16b8
manas mahanta <manas mahanta>
2016-11-13 11:36:46 +0530

    Initial commit

---
+++ app/src/main/java/com/techpalle/pushup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.techpalle.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void Testupload(){
+        int i=1+1;
+    }
+}
